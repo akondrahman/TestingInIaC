a8c17690d27460754d11760ae403802167266a6a
Akond Rahman <akond.rahman.buet@gmail.com>
2018-01-20 20:48:07 -0500

    search node for matches

--- sample_rspec_ast.rb
+++ sample_rspec_ast.rb
@@ -4,9 +4,18 @@ rspec tests for Puppet
 =end
 require 'rubocop'  ### need to import module 'RubCop'
 
-#code = '!something.empty?'
 ## now read file into string and let RuboCop parse it
 code_content = File.read('/Users/akond/Documents/AkondOneDrive/OneDrive/CSC712/project-materials/cisco-puppet-openstack-repo/spec/classes/openstack_compute_spec.rb')
 source = RuboCop::ProcessedSource.new(code_content, RUBY_VERSION.to_f)
 node = source.ast
-puts node  ### to see what AST it gives
+# puts node  ### to see what AST it gives
+=begin
+convert AST to str , search the string after that 
+=end
+node_as_str = node.to_s()
+param_matches = node_as_str.scan(/:params/)
+puts "Count of parameters: #{param_matches.length}"
+it_matches = node_as_str.scan(/:it/)
+puts "Count of it clauses: #{it_matches.length}"
+should_matches = node_as_str.scan(/:should/)
+puts "Count of should clauses: #{should_matches.length}"
