12e23cbef3cdc6725545d89a8d8ee342a33cbd99
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-25 16:33:51 -0400

    csc 712 project rq2

--- csc712_lint_engine.py
+++ csc712_lint_engine.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 import numpy as np
 import csc712_constants as constants
+import impl_aggr
 
 def getImplSmells(path2file):
     if(path2file.endswith(constants.PP_EXT)):
@@ -21,8 +22,8 @@ def runLinter(full_path_file):
     #1. run linter with custom rules
     getImplSmells(full_path_file)
     # 2. parse output
-    # all_rul_cnt_out = parseOutput()
+    impl_rul_cnt_out = impl_aggr.getImplSmellCount()
     #3. delete temp file
     # os.remove(constants.OUTPUT_TMP_LOG)
     ## returns a tuple: first is count, second is line string
-    # return all_rul_cnt_out
+    return impl_rul_cnt_out
