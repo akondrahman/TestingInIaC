8cc5e1b4991cf8696c16da1bbbffbaeda80f07cc
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-25 21:06:19 -0400

    csc 712 project rq2

--- csc712_extra_ost.py
+++ csc712_extra_ost.py
@@ -23,30 +23,39 @@ def getTimeInfo(id_param, repo_param):
 def getAllMonthsFromDataset(categ_file_param):
        str2write = ''
        df_list = []
-       '''
-       dicionary to hold files for each month
-       '''
-       month_file_dict, defect_file_dict, repo_file_dict = {}, {}, {}
        with open(categ_file_param, 'rU') as f:
          reader_ = csv.reader(f)
          next(reader_, None)
          for row in reader_:
              id_       = row[0]
              repo_     = row[1]
-             categ_    = row[2]
-             filepath_ = row[3]
-             size_     = row[4]
-             loc_      = row[5]
-             authors_  = row[6]
+             if 'Openstack' in categ_file_param:
+                 msg_       = 'DUMMY' # dummy variable
+                 categ_    = row[2]
+                 filepath_ = row[3]
+                 size_     = row[4]
+                 loc_      = row[5]
+                 authors_  = row[6]
+             else:
+                 msg_      = row[2]
+                 categ_    = row[3]
+                 filepath_ = row[4]
+                 size_     = row[6]
+                 loc_      = row[7]
+                 authors_  = row[8]
              time_ = getTimeInfo(id_, repo_)
              time2write = time_.split(' ')[0]
-             msg_       = 'DUMMY' # dummy variable
+
              df_list.append((id_, repo_, msg_, categ_, filepath_, time2write, size_, loc_, authors_))
        month_df = pd.DataFrame([x for x in df_list], columns=['msgid','repo', 'msgtxt', 'categ','filepath','date','size','loc','authorCount'])
-       print month_df.head()
-
+       # print month_df.head()
+       # month_df.to_csv('/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Wikimedia.Categ.For.CSC712.csv')
+       # month_df.to_csv('/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Openstack.Categ.For.CSC712.csv')
+       # month_df.to_csv('/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Mozilla.Categ.For.CSC712.csv')
 
 
 if __name__=='__main__':
-    categ_file_ = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Openstack.Categ.For.DB.csv'
+    # categ_file_ = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Mozilla.Final.Categ.csv'
+    # categ_file_ = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Openstack.Categ.For.DB.csv'
+    # categ_file_ = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Wikimedia.Final.Categ.csv'
     getAllMonthsFromDataset(categ_file_)
